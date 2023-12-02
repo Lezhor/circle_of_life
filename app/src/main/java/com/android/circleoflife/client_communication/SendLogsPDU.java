71650560cc5f2a9c0c0efbbf0f469bda4fb78550
Lezhor <dani.serdyuk@gmail.com>
2023-12-03 00:38:03 +0100

    Communication: Created SendLogsPDU with its testclass. however testclass can't be implemented before the log serializing is implemented...

---
+++ app/src/main/java/com/android/circleoflife/client_communication/SendLogsPDU.java
@@ -0,0 +1,77 @@
+package com.android.circleoflife.client_communication;
+
+import com.android.circleoflife.Application;
+import com.android.circleoflife.logging.model.Log;
+import com.android.circleoflife.logging.serializing.LogSerializer;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+/**
+ * Used to send logs to server.<br>
+ * The datablock contains an integer (how many Logs are there), followed by the serialisation of the logs.
+ * @see LogSerializer
+ */
+public class SendLogsPDU implements PDU {
+
+    final static int ID = 4;
+
+    private Log[] logArray;
+
+    /**
+     * Creates a pdu with logs - Note: There is no way to edit the logs later
+     */
+    SendLogsPDU(Log... logs) {
+        this.logArray = logs;
+    }
+
+    @Override
+    public int getID() {
+        return ID;
+    }
+
+    @Override
+    public void serialize(OutputStream os) throws IOException {
+        DataOutputStream dos = new DataOutputStream(os);
+        dos.writeInt(getID());
+        dos.writeInt(logArray.length);
+        LogSerializer serializer = Application.getLogSerializer();
+        for (Log log : logArray) {
+            serializer.serialize(os, log);
+        }
+    }
+
+    @Override
+    public void deserialize(InputStream is) throws IOException {
+        DataInputStream dis = new DataInputStream(is);
+        int logCount = dis.readInt();
+        Log[] logs = new Log[logCount];
+        LogSerializer serializer = Application.getLogSerializer();
+        for (int i = 0; i < logs.length; i++) {
+            logs[i] = serializer.deserialize(is);
+        }
+        this.logArray = logs;
+    }
+
+    /**
+     * Returns an instance of this class by calling {@link SendLogsPDU#deserialize(InputStream)}
+     * @param is Inputstream
+     * @return deserialized PDU
+     */
+    public static SendLogsPDU fromInputStream(InputStream is) throws IOException {
+        SendLogsPDU pdu = new SendLogsPDU();
+        pdu.deserialize(is);
+        return pdu;
+    }
+
+    /**
+     * Getter for the logs
+     * @return the log array
+     */
+    public Log[] getLogs() {
+        return logArray;
+    }
+}
---
+++ app/src/test/java/com/android/circleoflife/client_communication/SendLogsPDUTest.java
@@ -0,0 +1,64 @@
+package com.android.circleoflife.client_communication;
+
+import static org.junit.Assert.*;
+
+import com.android.circleoflife.logging.model.Log;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Tests {@link SendLogsPDU}
+ */
+public class SendLogsPDUTest {
+    Log[] logArray;
+
+    @Before
+    public void setUp() {
+        logArray = new Log[3];
+        // TODO: 03.12.2023 Test SendLogsPDU Setup
+    }
+
+    /**
+     * Tests serialize() method by serializing to ByteArrayOutputStream and then checking
+     * if the according inputstream.readInt() returns the needed pdu ID
+     */
+    @Test
+    public void testSerialize() {
+        // TODO: 03.12.2023 Test SendLogsPDU serialize
+        System.out.println("Testing SendLogsPDU Serializing");
+        try {
+            ByteArrayOutputStream os = new ByteArrayOutputStream();
+            PDU pdu = new SendLogsPDU(logArray);
+            pdu.serialize(os);
+            InputStream is = new ByteArrayInputStream(os.toByteArray());
+            DataInputStream dis = new DataInputStream(is);
+            assertEquals(pdu.getID(), dis.readInt());
+        } catch (IOException e) {
+            fail();
+        }
+    }
+
+    /**
+     * Checks if deserialisation works correctly with a given inputstream.<br>
+     * actually a joke-test because the pdu has no datablock...
+     */
+    @Test
+    public void testDeserialize() {
+        // TODO: 03.12.2023 Test SendLogsPDU deserialize
+        System.out.println("Testing SendLogsPDU Deserializing");
+        try {
+            InputStream is = new ByteArrayInputStream(new byte[0]);
+            PDU pdu = SendLogsPDU.fromInputStream(is);
+            assertEquals(SendLogsPDU.ID, pdu.getID());
+        } catch (IOException e) {
+            fail();
+        }
+    }
+}
