fd04d234934edbc35c04112c80ce62ec0413c919
Lezhor <dani.serdyuk@gmail.com>
2023-12-05 22:27:49 +0100

    Database: - Added androidx.room to dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,4 +41,7 @@ dependencies {
     //testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation("androidx.room:room-runtime:2.6.1")
+    annotationProcessor("androidx.room:room-compiler:2.6.1")
 }
