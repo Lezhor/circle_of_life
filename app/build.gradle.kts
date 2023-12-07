fc7c273e1a3bcd80904790ea9f01c625aed97159
Lezhor <dani.serdyuk@gmail.com>
2023-12-07 17:17:34 +0100

    build.gradle.kts - Tests that called methods that write Logcats do not fail anymore (because Log.d() now returns default value instead of throwing an exception)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,6 +36,10 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
+
+    testOptions.unitTests.apply {
+        isReturnDefaultValues = true
+    }
 }
 
 dependencies {
