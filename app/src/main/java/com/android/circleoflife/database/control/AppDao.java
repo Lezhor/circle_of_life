56f2f57472fccd464fa90f0d1c20c31eeee18a38
Lezhor <dani.serdyuk@gmail.com>
2023-12-09 00:09:40 +0100

    Database - Added executeQuery() to Dao - Changed all @Ignore methods to @Transaction methods

--- app/src/main/java/com/android/circleoflife/database/control/AppDao.java
+++ app/src/main/java/com/android/circleoflife/database/control/AppDao.java
@@ -1,13 +1,17 @@
 package com.android.circleoflife.database.control;
 
 import androidx.annotation.Nullable;
+import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Ignore;
 import androidx.room.Insert;
 import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
+import androidx.room.RawQuery;
+import androidx.room.Transaction;
 import androidx.room.Update;
+import androidx.sqlite.db.SupportSQLiteQuery;
 
 import com.android.circleoflife.database.models.*;
 
@@ -16,6 +20,9 @@ import java.util.List;
 @Dao
 interface AppDao {
     // TODO: 05.12.2023 Create DAO methods
+
+    @RawQuery(observedEntities = {Category.class, Cycle.class, Todo.class, Accomplishment.class})
+    LiveData<List<String>> executeQuery(SupportSQLiteQuery query);
 
 
     // ----------------------------------   User Methods   ----------------------------------------
@@ -49,7 +56,7 @@ interface AppDao {
     @Query("SELECT * FROM categories WHERE uid = :userID ORDER BY category_name")
     List<Category> getAllCategories(int userID);
 
-    @Ignore
+    @Transaction
     default List<Category> getAllCategories(User user) {
         return getAllCategories(user.getId());
     }
@@ -57,7 +64,7 @@ interface AppDao {
     @Query("SELECT * FROM categories WHERE uid = :userID AND parent_category = NULL ORDER BY category_name")
     List<Category> getRootCategories(int userID);
 
-    @Ignore
+    @Transaction
     default List<Category> getRootCategories(User user) {
         return getRootCategories(user.getId());
     }
@@ -66,7 +73,7 @@ interface AppDao {
     @Query("SELECT c1.uid, c1.category_name, c1.parent_category FROM categories AS c1 JOIN categories AS c2 ON c1.uid = c2.uid AND c1.category_name LIKE c2.parent_category AND c2.uid = :userID AND c2.category_name LIKE :categoryName LIMIT 1")
     Category getCategoryParent(int userID, String categoryName);
 
-    @Ignore
+    @Transaction
     default Category getParent(Category category) {
         return getCategoryParent(category.getUserID(), category.getName());
     }
@@ -74,7 +81,7 @@ interface AppDao {
     @Query("SELECT c1.uid, c1.category_name, c1.parent_category FROM categories AS c1 JOIN categories AS c2 ON c1.uid = c2.uid AND c1.parent_category LIKE c2.category_name AND c2.uid = :userID AND c2.category_name LIKE :categoryName ORDER BY c1.category_name")
     List<Category> getChildCategories(int userID, String categoryName);
 
-    @Ignore
+    @Transaction
     default List<Category> getChildCategories(Category category) {
         return getChildCategories(category.getUserID(), category.getName());
     }
