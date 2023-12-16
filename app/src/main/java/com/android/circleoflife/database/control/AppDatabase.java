2e56908252fd7bc314a954f22e282decff5df0f4
Lezhor <dani.serdyuk@gmail.com>
2023-12-16 14:14:20 +0100

    Database - Added sample user upon creation

--- app/src/main/java/com/android/circleoflife/database/control/AppDatabase.java
+++ app/src/main/java/com/android/circleoflife/database/control/AppDatabase.java
@@ -1,12 +1,16 @@
 package com.android.circleoflife.database.control;
 
 import android.content.Context;
+import android.util.Log;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 import androidx.room.TypeConverters;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
+import com.android.circleoflife.application.App;
 import com.android.circleoflife.database.control.daos.AccomplishmentDao;
 import com.android.circleoflife.database.control.daos.CategoryDao;
 import com.android.circleoflife.database.control.daos.CycleDao;
@@ -15,6 +19,11 @@ import com.android.circleoflife.database.control.daos.TodoDao;
 import com.android.circleoflife.database.control.daos.UserDao;
 import com.android.circleoflife.database.models.*;
 import com.android.circleoflife.database.models.type_converters.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * AppDatabase. offers the getDao() method which can perform actions on the database.
@@ -32,11 +41,18 @@ import com.android.circleoflife.database.models.type_converters.*;
 )
 @TypeConverters({LocalDateTimeConverter.class, DBLogConverter.class, CycleFrequencyConverter.class, UUIDConverter.class})
 public abstract class AppDatabase extends RoomDatabase {
+    private static final String TAG = "AppDatabase";
+
     abstract UserDao getUserDao();
+
     abstract CategoryDao getCategoryDao();
+
     abstract CycleDao getCycleDao();
+
     abstract TodoDao getTodoDao();
+
     abstract AccomplishmentDao getAccomplishmentDao();
+
     abstract LogDao getLogDao();
 
     /**
@@ -59,6 +75,7 @@ public abstract class AppDatabase extends RoomDatabase {
                                     AppDatabase.class,
                                     DATABASE_NAME
                             )
+                            .addCallback(defaultInit)
                             //.allowMainThreadQueries() // Stops checking for Main Thread.
                             //.fallbackToDestructiveMigration() // Creates new database when migration fails
                             .build();
@@ -68,5 +85,25 @@ public abstract class AppDatabase extends RoomDatabase {
         return instance;
     }
 
+    private static final RoomDatabase.Callback defaultInit = new RoomDatabase.Callback() {
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+            User user = new User(UUID.randomUUID(), "john_doe", "super_cool_password", LocalDateTime.now());
+            Category category = new Category(UUID.randomUUID(), "Sample Category", user.getId(), null);
+            Log.d(TAG, "Callback: inserting sample user: " + user);
+            UserDao userDao = getInstance(App.getApplicationContext()).getUserDao();
+            CategoryDao categoryDao = getInstance(App.getApplicationContext()).getCategoryDao();
+            ExecutorService service = Executors.newSingleThreadExecutor(r -> {
+                Thread t = new Thread(r);
+                t.setDaemon(true);
+                return t;
+            });
+            service.execute(() -> {
+                userDao.insert(user);
+                categoryDao.insert(category);
+            });
+        }
+    };
 
 }
