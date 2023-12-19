2a0f37538a92f646666b89ed30ccec9e2e4073aa
Lezhor <dani.serdyuk@gmail.com>
2023-12-19 19:00:59 +0100

    UI: Action bar has right colors

--- app/src/main/java/com/android/circleoflife/ui/activities/categories/RootCategoriesActivity.java
+++ app/src/main/java/com/android/circleoflife/ui/activities/categories/RootCategoriesActivity.java
@@ -1,12 +1,22 @@
 package com.android.circleoflife.ui.activities.categories;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.util.Log;
+import android.util.TypedValue;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.WindowManager;
 import android.widget.Toast;
 
 import com.android.circleoflife.R;
@@ -47,5 +57,34 @@ public class RootCategoriesActivity extends AppCompatActivity {
 
         //categoryViewModel.getCurrentCategories().observe(this, categories -> Toast.makeText(this, categories.stream().map(Category::toString).reduce("Categories changed: ", (a, b) -> a + b + ";"), Toast.LENGTH_SHORT).show());
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.categories_root_toolbar_menu, menu);
+        TypedValue typedValue = new TypedValue();
+        Resources.Theme theme = getTheme();
+        theme.resolveAttribute(android.R.attr.textColorPrimary, typedValue, true);
+        TypedArray arr = obtainStyledAttributes(typedValue.data, new int[]{android.R.attr.textColorSecondary});
+        int primaryColor = arr.getColor(0, -1);
+        for (int i = 0; i < menu.size(); i++) {
+            Drawable drawable = menu.getItem(i).getIcon();
+            if (drawable != null) {
+                drawable.mutate();
+                drawable.setColorFilter(primaryColor, PorterDuff.Mode.SRC_ATOP);
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.search_button) {
+            Toast.makeText(this, "Searching...", Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.add_category) {
+            Toast.makeText(this, "Creating new...", Toast.LENGTH_SHORT).show();
+        }
+        return true;
     }
 }
