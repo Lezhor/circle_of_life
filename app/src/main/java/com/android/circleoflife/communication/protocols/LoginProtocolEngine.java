f8f9fd1288441449ee141f6edaf6d2883a186d2b
Lezhor <dani.serdyuk@gmail.com>
2024-01-02 20:16:39 +0100

    Communication: Implemented LoginProtocolEngine method

--- app/src/main/java/com/android/circleoflife/communication/protocols/LoginProtocolEngine.java
+++ app/src/main/java/com/android/circleoflife/communication/protocols/LoginProtocolEngine.java
@@ -1,8 +1,19 @@
 package com.android.circleoflife.communication.protocols;
 
+import android.util.Log;
+
+import com.android.circleoflife.application.App;
+import com.android.circleoflife.communication.pdus.LoginFailedPDU;
+import com.android.circleoflife.communication.pdus.LoginPDU;
+import com.android.circleoflife.communication.pdus.PDU;
+import com.android.circleoflife.communication.pdus.SendUserPDU;
+import com.android.circleoflife.communication.socket_communication.SocketCommunication;
 import com.android.circleoflife.database.models.User;
 
+import java.io.IOException;
+
 public class LoginProtocolEngine implements LoginProtocol {
+    private static final String TAG = LoginProtocolEngine.class.getSimpleName();
 
 
     private static volatile LoginProtocolEngine instance;
@@ -34,7 +45,37 @@ public class LoginProtocolEngine implements LoginProtocol {
 
     @Override
     public User login(String username, String password) {
-        // TODO: 02.01.2024 Login protocol method!
+        Log.d(TAG, "Begin syncing...");
+        SocketCommunication com = App.openCommunicationSessionWithServer();
+        try {
+            com.connectToServer();
+        } catch (IOException e) {
+            Log.w(TAG, "Connection to server failed");
+            return null;
+        }
+        try {
+            ProtocolSerializer serializer = new ProtocolSerializer(this, com);
+
+            // Step 1:
+            LoginPDU loginPDU = new LoginPDU(username, password);
+            Log.d(TAG, "1) Sending LoginPDU");
+            serializer.serialize(loginPDU);
+
+            // Step 2:
+            PDU pdu = serializer.deserialize();
+            Log.d(TAG, "2) received PDU with ID: " + pdu.getID());
+            if (pdu instanceof SendUserPDU sendUserPDU) {
+                Log.d(TAG, "Returning user: " + sendUserPDU.getUser());
+                return sendUserPDU.getUser();
+            } else if (pdu instanceof LoginFailedPDU) {
+                Log.i(TAG, "login failed!");
+            }
+
+        } catch (IOException e) {
+            Log.w(TAG, "Communication failed", e);
+        } finally {
+            com.disconnectFromServer();
+        }
         return null;
     }
 
