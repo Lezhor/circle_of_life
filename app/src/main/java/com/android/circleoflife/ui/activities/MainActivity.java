b41b1c43661f7f6cacc89ecf6f401177a3a622d5
Lezhor <dani.serdyuk@gmail.com>
2024-01-09 16:21:46 +0100

    UI: Updated from EditText to TextInputLayout

--- app/src/main/java/com/android/circleoflife/ui/activities/MainActivity.java
+++ app/src/main/java/com/android/circleoflife/ui/activities/MainActivity.java
@@ -4,13 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Toast;
 
 import com.android.circleoflife.R;
 import com.android.circleoflife.application.App;
 import com.android.circleoflife.database.validators.StringValidator;
 import com.android.circleoflife.ui.activities.categories.root.RootCategoriesActivity;
+import com.android.circleoflife.ui.other.TextInputLayoutValidator;
+import com.google.android.material.textfield.TextInputLayout;
 
 import java.io.IOException;
 import java.util.Arrays;
@@ -19,8 +20,8 @@ public class MainActivity extends SuperActivity {
 
     private static final String TAG = "MainActivity";
 
-    private EditText usernameEdit;
-    private EditText passwordEdit;
+    private TextInputLayout usernameInput;
+    private TextInputLayout passwordInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,14 +31,14 @@ public class MainActivity extends SuperActivity {
         Button button = findViewById(R.id.login_button);
         button.setOnClickListener(v -> login());
 
-        usernameEdit = findViewById(R.id.edit_username);
-        passwordEdit = findViewById(R.id.edit_password);
+        usernameInput = findViewById(R.id.edit_username);
+        passwordInput = findViewById(R.id.edit_password);
 
         Log.d(TAG, "temp: " + "Test");
         new Thread(() -> Log.d(TAG, "temp: " + Arrays.stream(App.getResources().getStringArray(R.array.days_of_week)).reduce("", (a, b) -> a + b + "; ")))
                 .start();
 
-        tryLoginWithLastDate();
+        //tryLoginWithLastDate(); todo - comment in
 
         executeInBackground(() -> App.getDatabaseController().getUserByUsername("john_doe"),
                 user -> Log.d(TAG, "username: '" + user.getUsername() + "', password: '" + user.getPassword() + "'"));
@@ -65,39 +66,37 @@ public class MainActivity extends SuperActivity {
     }
 
     private void login() {
-        String username = usernameEdit.getText().toString();
-        String password = passwordEdit.getText().toString();
+        String username = usernameInput.getEditText().getText().toString();
+        String password = passwordInput.getEditText().getText().toString();
         Log.d(TAG, "login: tries to login with username: " + username + ", password: " + password);
-        try {
-            StringValidator.validateUsername(username);
-            StringValidator.validatePassword(password);
-        } catch (IllegalArgumentException e) {
-            Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
-            return;
-        }
-        executeInBackground(
-                () -> {
-                    try {
-                        if (App.getAuthentication().login(username, password)) {
-                            return null;
+        if (
+                TextInputLayoutValidator.validate(usernameInput, StringValidator::validateUsername, getString(R.string.username_hint))
+                & TextInputLayoutValidator.validate(passwordInput, StringValidator::validatePassword, getString(R.string.password_hint))
+        ) {
+            executeInBackground(
+                    () -> {
+                        try {
+                            if (App.getAuthentication().login(username, password)) {
+                                return null;
+                            } else {
+                                return "Login failed";
+                            }
+                        } catch (IOException e) {
+                            return "Login failed: " + e.getMessage();
+                        }
+                    },
+                    str -> {
+                        if (str == null) {
+                            // login succeeded
+                            if (App.getAuthentication().getUser() != null) {
+                                goToNextActivity();
+                            }
                         } else {
-                            return "Login failed";
+                            Toast.makeText(this, str, Toast.LENGTH_SHORT).show();
                         }
-                    } catch (IOException e) {
-                        return "Login failed: " + e.getMessage();
                     }
-                },
-                str -> {
-                    if (str == null) {
-                        // login succeeded
-                        if (App.getAuthentication().getUser() != null) {
-                            goToNextActivity();
-                        }
-                    } else {
-                        Toast.makeText(this, str, Toast.LENGTH_SHORT).show();
-                    }
-                }
-        );
+            );
+        }
 
     }
 
