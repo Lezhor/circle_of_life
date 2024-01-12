3d08e1b5fee5f8ada621f5802321101f60dfceb6
Lezhor <dani.serdyuk@gmail.com>
2024-01-12 11:48:03 +0100

    Espresso: Added "androidx.test.espresso:espresso-contrib" as androidImplementation to build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,7 +44,7 @@ android {
 
 dependencies {
     val lifecycle_version = "2.6.2"
-    val room_version = "2.6.1";
+    val room_version = "2.6.1"
     val arch_version = "2.2.0"
 
     implementation("androidx.appcompat:appcompat:1.6.1")
@@ -55,6 +55,7 @@ dependencies {
     //testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.espresso:espresso-contrib:3.5.1")
     androidTestImplementation("androidx.test:runner:1.5.2")
 
     implementation("com.getbase:floatingactionbutton:1.10.1")
